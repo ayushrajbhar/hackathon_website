b11383cba1a5fbadfde0eeb2f291a2668951e8be
Ayush Rajbhar <ayushrajbhar026@gmail.com>
2024-02-11 01:38:44 +0530

    Vercel web analytics changes

--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { Analytics } from '@vercel/analytics/react';
+import { inject } from '@vercel/analytics';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+inject();
 root.render(
   <React.StrictMode>
     <App />
-    <Analytics />
   </React.StrictMode>
 );
 
